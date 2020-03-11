1c67f011df97762ea12e1a2f580529cb1ccf54a2
Joris Bosch <j.n.bosch@students.uu.nl>
2020-03-11 16:16:29 +0100

    Added fungus & platform class

---
+++ Oakheart/Game1/Map/Platform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using OakHeart;
+
+class Platform : SpriteGameObject
+{
+    List<Fungus> fungusList;
+    Rectangle boundingBox;
+    Player player;
+    public Platform(bool infected, float rotation, string assetName, Rectangle boundingBox, int layer, string id) : base(rotation, assetName, layer, id)
+    {
+        this.boundingBox = boundingBox;
+        position = new Vector2(boundingBox.X, boundingBox.Y);
+        if (infected)
+        {
+            fungusList = new List<Fungus>();
+            for (int i = 0; boundingBox.Width / 180 > fungusList.Count; i++)
+            {
+                fungusList.Add(new Fungus());
+            } 
+        }
+
+    }
+}
+    
+    
