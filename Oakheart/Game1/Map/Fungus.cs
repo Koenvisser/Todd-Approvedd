7774831c6a03cdb663dd8390169cd6ac0dfc187b
Magnusly <joris.bosch@proximus.be>
2020-03-16 06:32:34 +0100

    Updates to Fungus & platforms

--- Oakheart/Game1/Map/Fungus.cs
+++ Oakheart/Game1/Map/Fungus.cs
@@ -3,12 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-class Fungus
+class Fungus : SpriteGameObject
 {
-    public Fungus()
+    float rotation;
+    bool cleansed;
+    Player player;
+    public Fungus(float rotation, string assetName, int index, Vector2 platformPosition, Player player, int layer = 0, string id = "") : base(rotation, assetName, layer, id)
     {
+        position = platformPosition + new Vector2(index * player.Sprite.Width, 0);
+        origin = platformPosition;
+        this.rotation = rotation;
+        this.player = player;
+    }
 
+    public override void Update(GameTime gameTime)
+    {
+        if (!cleansed)
+        {
+            if (player.position.X - position.X < 10 && && player)
+        }
+    }
+
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        if (!cleansed)
+        {
+            spriteBatch.Draw(sprite.Sprite, position, null, Color.White, rotation, origin, 1, SpriteEffects.None, 0);
+        }        
     }
 }
 
--- Oakheart/Game1/Map/Platform.cs
+++ Oakheart/Game1/Map/Platform.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using OakHeart;
 
 class Platform : SpriteGameObject
@@ -11,19 +12,34 @@ class Platform : SpriteGameObject
     List<Fungus> fungusList;
     Rectangle boundingBox;
     Player player;
-    public Platform(bool infected, float rotation, string assetName, Rectangle boundingBox, int layer, string id) : base(rotation, assetName, layer, id)
+    float rotation;
+    public Platform(bool infected, float rotation, string assetName, Rectangle boundingBox, Player player, int layer, string id) : base(rotation, assetName, layer, id)
     {
         this.boundingBox = boundingBox;
+        this.rotation = rotation;
+        this.player = player;
         position = new Vector2(boundingBox.X, boundingBox.Y);
         if (infected)
         {
             fungusList = new List<Fungus>();
-            for (int i = 0; boundingBox.Width / 180 > fungusList.Count; i++)
+            for (int i = 0; boundingBox.Width / player.Sprite.Width > fungusList.Count; i++)
             {
-                fungusList.Add(new Fungus());
+                fungusList.Add(new Fungus(rotation, "fungus", i, position, player));
             } 
         }
 
+    }
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(sprite.Sprite, boundingBox, null, Color.White, rotation, new Vector2(0), SpriteEffects.None, 0);
+
+        if(fungusList != null)
+        {
+            for(int i = 0; i < fungusList.Count; i++)
+            {
+                fungusList[i].Draw(gameTime, spriteBatch);
+            }
+        }
     }
 }
     
