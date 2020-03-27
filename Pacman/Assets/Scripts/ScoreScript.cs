45513e9e6886fb65da5af8eb9972691bc5065924
Koenvisser <KoenVisser2000@gmail.com>
2020-03-27 15:41:39 +0100

    added popup

--- Pacman/Assets/Scripts/ScoreScript.cs
+++ Pacman/Assets/Scripts/ScoreScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,12 +7,15 @@ using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
+    public GameObject Popup;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI HighscoreText;
     public TextMeshProUGUI CurrencyText;
     public int score;
     private int Highscore;
-    public double expectedScore = 0;
+    private float timer;
+    private double expectedScore = 0;
+    public GameObject PacMan;
 
     public void Start()
     {
@@ -21,13 +25,15 @@ public class ScoreScript : MonoBehaviour
 
     public void ScorePellet()
     {
-        float popupChance = Random.value;
+        float popupChance = UnityEngine.Random.value;
         score += 10;
         scoreText.text = score.ToString();
         SetHighScoreText(score);
         if (popupChance < 0.01f)
         {
-            //popup callen
+            Popup.SetActive(true);
+            Popup.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().SetText("You need to pay " + Math.Ceiling(expectedScore) + " to continue");
+            PacMan.GetComponent<PacManMoveScript>().enabled = false;
         }
     }
 
@@ -59,6 +65,7 @@ public class ScoreScript : MonoBehaviour
         }
     }
 
+    
     public void SetHighscore()
     {
         if (score > GetHighScore())
@@ -68,15 +75,36 @@ public class ScoreScript : MonoBehaviour
     }
     void Update()
     {
-        if (score < 1000)
+        if (Popup.activeSelf == false)
         {
-            expectedScore += 0.03 * Time.time;
-        }
+            if (score < 1000 && Popup.activeSelf == false)
+            {
+                expectedScore += 0.03 * Time.time;
+            }
 
-        if (score > 1000)
+            if (score > 1000 && Popup.activeSelf == false)
+            {
+                expectedScore += 0.01 * Time.time;
+            } 
+        }
+        else
         {
-            expectedScore += 0.01 * Time.time;
+            timer += Time.deltaTime;
+            if (timer % 1 > .5f)
+            {
+                Popup.transform.GetChild(2).gameObject.SetActive(false);
+            }
+            else
+            {
+                Popup.transform.GetChild(2).gameObject.SetActive(true);
+            }
+            if (Input.GetKeyDown("space") && score >= expectedScore)
+            {
+                score -= (int)Math.Ceiling(expectedScore);
+                scoreText.text = score.ToString();
+                Popup.SetActive(false);
+                PacMan.GetComponent<PacManMoveScript>().enabled = true;
+            }
         }
-
     }
 }
